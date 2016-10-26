77302d64d3f77816749301505f0738efbaf19f9b
Arman Ortega <ortegaaa@ph.ibm.com>
2016-10-26 15:39:15 +0800

    Update src/BookStore.js

--- src/BookStore.js
+++ src/BookStore.js
@@ -5,48 +5,98 @@ var BookStore = React.createClass({
     return ({ currentStep: 1 });
   },
 
+  updateFormData(formData) {
+    console.log('updateFormData:', JSON.stringify(formData));
+    var formValues = Object.assign({}, this.state.formValues, formData);
+
+    var nextStep = this.state.currentStep + 1;
+    this.setState({formValues: formValues, currentStep: nextStep});
+  },
+
   render() {
     switch (this.state.currentStep) {
       case 1:
-        return <BookList />;
+        return <BookList parentUpdateFormData={this.updateFormData} />;
       case 2:
-        return <ShippingDetails />;
+        return <ShippingDetails updateFormData={this.updateFormData} />;
       case 3:
-        return <DeliveryDetails />;
+        return <DeliveryDetails updateFormData={this.updateFormData} />;
     }
   }
 });
 
+// Step 1 of 3
 var BookList = React.createClass({
   getInitialState() {
     return ( {
-      books: [{ name: 'Zero to One', author: 'Peter Thiel' },
-              { name: 'Monk who sold his Ferrari', author: 'Robin Sharma' },
-              { name: 'Wings of Fire', author: 'A.P.J. Abdul Kalam' }
-            ] }
-    )
+      books: [{ id:1, name: 'Zero to One', author: 'Peter Thiel' },
+              { id:2, name: 'Monk who sold his Ferrari', author: 'Robin Sharma' },
+              { id:3, name: 'Wings of Fire', author: 'A.P.J. Abdul Kalam' }
+            ],
+              selectedBooks: [],
+              error: false
+            }
+    );
+  },
+
+  _renderError() {
+    if (this.state.error) {
+      return (
+        <div className="alert alert-danger">
+          {this.state.error}
+        </div>
+      );
+    }
   },
 
   _renderBook(book) {
     return(
-      <div className="checkbox">
+      <div className="checkbox" key={book.id}>
         <label>
-          <input type="checkbox" /> {book.name} -- {book.author}
+          <input type="checkbox" value={book.name} onChange={this.handleSelectedBooks} /> {book.name} -- {book.author}
         </label>
       </div>
     );
   },
 
+  handleSelectedBooks(event){
+    var selbooks = this.state.selectedBooks;
+    console.log('QQQ selectedBooks:', selbooks);
+    var index = selbooks.indexOf(event.target.value);
+    console.log('QQQ index:', index);
+    if (event.target.checked) {
+      console.log('checked!');
+      if (index === -1) {
+        selbooks.push(event.target.value);
+      }
+    } else {
+      selbooks.splice(index, 1);
+    }
+
+    this.setState({selectedBooks: selbooks });
+  },
+
   handleSubmit(event) {
-      console.log(event);
-      event.preventDefault();
-      console.log("Form submitted");
+    console.log(event);
+    event.preventDefault();
+    console.log("Form submitted");
+    var selbooks = this.state.selectedBooks;
+    console.log('QQQ selectedBooks:', selbooks);
+    if (selbooks.length === 0) {
+      this.setState({error: 'Please choose at least one book to continue'});
+    } else {
+      this.setState({error: false});
+      this.props.parentUpdateFormData({selectedBooks: this.state.selectedBooks});
+    }
+
   },
 
   render() {
+    var errorMessage = this._renderError();
     return(
       <div>
         <h3>Choose from wide variety of books available in our store.</h3>
+        {errorMessage}
         <form onSubmit={this.handleSubmit}>
           {this.state.books.map((book) => {
              return this._renderBook(book); })
@@ -60,6 +110,7 @@ var BookList = React.createClass({
   }
 });
 
+// Step 2 of 3
 var ShippingDetails = React.createClass({
   render() {
     return(
@@ -68,6 +119,7 @@ var ShippingDetails = React.createClass({
   }
 });
 
+// Step 3 of 3
 var DeliveryDetails = React.createClass({
   render() {
     return (
