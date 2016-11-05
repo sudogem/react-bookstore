f78dff95f77854990af3b8980e8a74a0e7093bc3
Arman Ortega <ortegaaa@ph.ibm.com>
2016-11-05 16:22:16 +0800

    Fix the path on serving the bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
 var path = require('path');
-var webpack = require('webpack')
+var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index',
-    // output: {
-    //   path: path.join(__dirname, 'dist'),
-    //   path: '/',
-    //   filename: 'bundle.js',
-    //   publicPath: '/static/'
-    // },
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js'
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    module: {
-      loaders: [
-        {
-          test: /.jsx?$/,
-          loader: 'babel-loader',
-          include: path.join(__dirname, 'src'),
-          exclude: /(node_modules|bower_components)/,
-          query: {
-            presets: ['es2015', 'react']
-          }
+  entry: './src/index',
+  // store the generated files at /build/bundle.js
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015', 'react']
         }
-      ]
-    }
+      }
+    ]
+  }
 }
